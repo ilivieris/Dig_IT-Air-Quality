3418e247633908d7e3d3d2ef09d0ff9e2385c998
ilivieris <ilivieris@core-innovation.com>
2022-10-10 16:00:18 +0300

    Considerable updates

--- CO/utils/NBeats_Keras.py
+++ CO/utils/NBeats_Keras.py
@@ -15,7 +15,17 @@ def swish(x, beta = 1):
 
         
 get_custom_objects().update({'swish': Activation(swish)})
-        
+
+
+
+# from tensorflow.keras.utils  import get_custom_objects
+
+def gelu(x):
+    return 0.5 * x * (1 + tf.tanh(tf.sqrt(2 / np.pi) * (x + 0.044715 * x^3)))
+    # return tf.keras.backend.hard_sigmoid(1.702 * x) * x
+
+get_custom_objects().update({'gelu':  Activation(gelu)})
+
 
 
 class NBeatsNet:
--- NO2/utils/plot_scatter.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def plot_scatter(*args, **kwargs):
-    plt.plot(*args, **kwargs)
-    plt.scatter(*args, **kwargs)
